67eb1d7ef856ac1e87ae1878d9446c4a07f7204b
ci <ci>
2020-11-05 16:13:53 +0000

    init

---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["filetime"] = [{"text":"impl StructuralEq for FileTime","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
